e3f0206b588b482bc38e949a8e9831c32802799c
SpyrosGeo <xarodasimba@gmail.com>
2020-03-13 19:10:00 +0200

    finished markup for Join component and added css

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import './styles/Join.css'
 
 export default function Join() {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return (
-        <div>
-            <h1>test</h1>
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join Pchat</h1>
+                <div><input placeholder=""
+                    className="joinInput mt-20"
+                    type="text"
+                    onChange={(e)=>setName(e.target.value)} />
+                </div>
+                <div><input placeholder=""
+                    className="joinInput"
+                    type="text"
+                    onChange={(e)=>setRoom(e.target.value)} />
+                </div>
+                <Link onClick={e=>(!name || !room)?e.preventDefault():null} to={`/chat?name=${name}&room=${room}`} >
+                    <button type="submit" className="button mt-20">
+                        Sign in
+                    </button>
+                </Link>
+            </div>
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -3,11 +3,28 @@ const socket = require('socket.io');
 const http = require('http');
 
 const PORT = process.env.PORT || 5000
+
+//import router from router.js
+const router = require('./router')
+
 //socket.io server initialize
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
+io.on('connection',socket=>{
+        console.log("We have a new connection")
+
+    socket.on('disconnect',()=>{
+        console.log("user has left")
+    })
+
+
+})
+
+
+//use the router as middleware
+app.use(router);
 
 server.listen(PORT,()=>{
     console.log(`server has started on port ${PORT}`)
