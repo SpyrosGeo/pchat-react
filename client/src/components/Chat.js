551218b323c013d6d6f24e7d0f636cfbe96ba7cb
SpyrosGeo <xarodasimba@gmail.com>
2020-03-13 21:01:07 +0200

    implemented connection between input and backend

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react'
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import './styles/Chat.css'
 let socket;
 export default function Chat({ location }) {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = "localhost:5000";
 
     useEffect(() => {
@@ -15,19 +18,43 @@ export default function Chat({ location }) {
         setName(name);
         setRoom(room);
         //emit events
-        socket.emit('join', { name, room },({err})=>{
+        socket.emit('join', { name, room }, () => {
 
         })
         //handle disconnect
-        return()=>{
+        return () => {
             socket.emit('disconnect');
 
             socket.off();
         }
     }, [ENDPOINT, location.search])
+
+
+
+    useEffect(() => {
+        //listen for messages
+        socket.on('message', (message) => {
+            setMessages([...messages, message])
+        })
+    }, [messages])
+
+    const sendMessage = (e) => {
+        e.preventDefault()
+        if (message) {
+            console.log('MESSAGE')
+            socket.emit('sendMessage', message, () => setMessage(''))
+        }
+    }
+
+    console.log(message,messages);
     return (
-        <div>
-            <h1>Chat Component</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <input
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
+                    onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null} />
+            </div>
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
 
+//import helper fuctions
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
+
 const PORT = process.env.PORT || 5000
 
 //import router from router.js
@@ -12,14 +15,32 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
-io.on('connection',socket=>{
-        console.log("We have a new connection")
+//use the router as middleware
+app.use(router);
+io.on('connection', socket => {
+    socket.on('join', ({ name, room }, callback) => {
+        const { err, user } = addUser({ id: socket.id, name, room });
 
-        socket.on('join',({name,room},callback)=>{
-            console.log(name)
-          
-        })
-    socket.on('disconnect',()=>{
+        if (err) {
+            return callback(err)
+        }
+        //emit message from back-end as admin
+        socket.emit('message', { user: 'admin', text: `${user.name},Welcome to the ${user.room} room` })
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined.` })
+        socket.join(user.room);
+
+        callback();
+
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', { user: user.name, text: message })
+        
+        callback();
+    })
+
+    socket.on('disconnect', () => {
         console.log("user has left")
     })
 
@@ -27,9 +48,7 @@ io.on('connection',socket=>{
 })
 
 
-//use the router as middleware
-app.use(router);
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`server has started on port ${PORT}`)
 })
