7333ae351f54dbd07230228076673abba8d4fc0e
SpyrosGeo <xarodasimba@gmail.com>
2020-03-14 00:25:48 +0200

    added TextContainer component added finishing touches

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -6,10 +6,12 @@ import './styles/Chat.css'
 import InfoBar from './InfoBar';
 import Input from './Input';
 import Messages from './Messages';
+import TextContainer from './TextContainer';
 let socket;
 export default function Chat({ location }) {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('')
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const ENDPOINT = "localhost:5000";
@@ -39,6 +41,9 @@ export default function Chat({ location }) {
         socket.on('message', (message) => {
             setMessages([...messages, message])
         })
+        socket.on('roomData', ({ users })=>{
+            setUsers(users)
+        })
     }, [messages])
 
     const sendMessage = (e) => {
@@ -54,9 +59,10 @@ export default function Chat({ location }) {
         <div className="outerContainer">
             <div className="container">
                 <InfoBar room={room} />
-                <Messages messages={messages} name={name}/>
+                <Messages messages={messages} name={name} />
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
+            <TextContainer users={users}  />
         </div>
     )
 }
